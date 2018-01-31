cacf03f398c4a0ad8c0ff760399377c0fe361b68
Roberto San Martin <rsanmartin@tinet.cl>
2018-01-31 01:13:54 -0300

    Se incorpora archivos de procesamiento por lotes.

---
+++ pdf417-barcode-gen.bat
@@ -0,0 +1,27 @@
+@echo off
+
+rem Parametros de configuracion del programa.
+set FORMATO=png
+set ROWS=5
+set COLUMNS=18
+set ERROR_LEVEL=5
+set LEN_CODEWORDS=999
+set OPTIONS=32
+set COLOR=0x0000
+set BG_COLOR=0xFFFF
+set ENCODING=ISO-8859-1
+
+set PARAM1=%~1
+set PARAM2=%~2
+
+if not defined PARAM1 goto Error
+if not defined PARAM2 goto Error
+
+rem Ejecucion del programa.
+java -jar target/pdf17-barcode-gen-1.0-SNAPSHOT-jar-with-dependencies.jar %1 %2 %FORMATO% %ROWS% %COLUMNS% %ERROR_LEVEL% %LEN_CODEWORDS% %OPTIONS% %COLOR% %BG_COLOR% %ENCODING%
+goto Fin
+
+:Error
+java -jar target/pdf17-barcode-gen-1.0-SNAPSHOT-jar-with-dependencies.jar
+
+:Fin
---
+++ pdf417-barcode-gen.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Parametros de configuracion del programa.
+FORMATO=png
+ROWS=5
+COLUMNS=18
+ERROR_LEVEL=5
+LEN_CODEWORDS=999
+OPTIONS=32
+COLOR=0x0000
+BG_COLOR=0xFFFF
+ENCODING=ISO-8859-1
+
+if [ "$#" -eq 2 ]; then
+	# Ejecucion del programa.
+	java -jar target/pdf17-barcode-gen-1.0-SNAPSHOT-jar-with-dependencies.jar ${1} ${2} ${FORMATO} ${ROWS} ${COLUMNS} ${ERROR_LEVEL} ${LEN_CODEWORDS} ${OPTIONS} ${COLOR} ${BG_COLOR} ${ENCODING}
+else
+	java -jar target/pdf17-barcode-gen-1.0-SNAPSHOT-jar-with-dependencies.jar
+fi
